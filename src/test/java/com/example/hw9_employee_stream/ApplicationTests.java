115ba80f138e5cd590dc86b7c8833241bc5ab615
anna-ermakova <korovanna8@mail.ru>
2023-04-22 20:39:39 +0300

    Initial commit

---
+++ src/test/java/com/example/hw9_employee_stream/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw9_employee_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
